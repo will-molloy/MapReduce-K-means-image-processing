fd4391ea930141bde34dc4effa58ba07ffc069e5
Will Molloy <willjoemolloy@gmail.com>
2018-05-14 11:02:11 +1200

    Make clear what proj is reddit comment/kmeans (#5)

--- mapreduce-tutorials/transient/src/master.hs
+++
@@ -1,8 +0,0 @@
-import Transient.Move (runCloudIO, lliftIO, createNode, connect, getNodes, onAll)
-
-main = runCloudIO $ do
-    this   <- lliftIO (createNode lh 8000)
-    master <- lliftIO (createNode lh 8001)
-    connect this master
-    onAll getNodes >>= lliftIO . putStrLn . show
-    where lh = "0.0.0.0"
